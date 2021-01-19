5b9f3845f0db8da8171eabea0b4ba4fa793574d7
SIMPLON <tellai>
2021-01-19 20:37:55 +0100

    spotify+slider

--- web/form_js/form.js
+++ web/form_js/form.js
@@ -1,7 +1,61 @@
 let listName = [];
-let myP = document.querySelector("#myP");
+let divList = document.querySelector(".list");
+let divGrp = document.querySelector(".grp");
 
-document.querySelector("#push").addEventListener("click", function() {
-    let myName = document.getElementById("#fname");
-    myP.innerText = myName;
-})
+
+function create_list() {
+  let myName = document.querySelector("#name").value;
+  listName.push(myName);
+  let mylist = document.createElement("div");
+  mylist.innerText += myName + "\n";
+  divList.appendChild(mylist);
+}
+function swapArray(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+let new_grp = [];
+let nb_par_grp = document.querySelector("#nb_grp").value;
+function nb_input() {
+  
+  let exp=new RegExp("[0-9]");
+  nb_par_grp = parseInt(nb_par_grp);
+
+  if (exp.test(nb_par_grp) == true) {
+    create_grp();
+  } else {
+    //alert("Attention, votre saisie est incorrect !");
+  }
+  swapArray(listName);
+  return nb_par_grp;
+}
+console.log(listName);
+console.log(nb_par_grp);
+//let nb_par_grp = nb_input();
+function create_grp() {
+    let count_Grp = 1;
+  while (listName.length > 0) {
+    let myGrp = document.createElement("div");
+    myGrp.innerText += "Groupe" + count_Grp + " : " + "\n";
+    new_grp = listName.splice(0, nb_par_grp);
+
+    for (var i = 0; i < new_grp.length; i++) {
+      myGrp.innerText += new_grp[i] + "\n";
+
+      divGrp.appendChild(myGrp);
+    }
+    count_Grp++;
+  }
+}
+console.log(nb_par_grp);
+
+function app_delete() {
+  let deleteList = listName.splice(0, listName.length);
+  deletelist = new_grp.splice(0, new_grp.length);
+  divGrp.innerText = new_grp;
+  divList.innerText = new_grp;
+}
--- web/projet_spotify/audio.js
+++ web/projet_spotify/audio.js
@@ -1,58 +1,97 @@
 let playButton = document.querySelector(".play");
-let pauseButton = document.querySelector(".pause");
 let suivantButton = document.querySelector(".suivant");
 let precedentButton = document.querySelector(".precedent");
 let imageTrack = document.querySelector(".track_image");
 let artist = document.querySelector(".artist");
 let title = document.querySelector(".title");
 let volume_show = document.querySelector("#volume_show");
+let recent_volume = document.querySelector("#volume");
+let show_duration = document.querySelector("#show_duration");
+let slider = document.querySelector("#duration_slider");
+let track_duration = document.querySelector("#track_duration");
 let track = document.createElement("audio");
 
-let num_index = 1;
+let num_index = 0;
 let playing_song = false;
 
 let all_song = [
   {
     name: "Chacun ses raisons",
     path: "music/chacun-ses-raisons.mp3",
-    picture: "img/NIRO.jpg",
+    picture: "img/niro.png",
     singer: "NIRO",
   },
   {
-    name: "500euros",
+    name: "500€",
     path: "music/rimk-500-eur.mp3",
     picture: "img/rimk.jpg",
     singer: "RIMK",
   },
   {
+    name: "La fierté des nôtres",
+    path: "music/fierte.mp3",
+    picture: "img/rohff.jpg",
+    singer: "ROHFF",
+  },
+  {
+    name: "La tête dans les nuages",
+    path: "music/nuage.mp3",
+    picture: "img/jul2.jpg",
+    singer: "JUL",
+  },
+  {
+    name: "Esmeralda",
+    path: "music/ezzahi.mp3",
+    picture: "img/ammar_ezzahi.jpeg",
+    singer: "AMMAR EZZAHI",
+  },
+  {
     name: "California love",
     path: "music/california-love.mp3",
-    picture: "img/tupac.jpg",
+    picture: "img/tupac2.jpg",
     singer: "TUPAC",
   },
   {
     name: "Temps d'avant",
     path: "music/temps-davant.mp3",
-    picture: "img/jul.jpg",
+    picture: "img/jul2.jpg",
     singer: "JUL",
   },
   {
+    name: "Imagine",
+    path: "music/imagine.mp3",
+    picture: "img/john_lennon.jpg",
+    singer: "JOHN LENNON",
+  },
+  {
+    name: "Hotel California",
+    path: "music/hotel-california-2013-remaster.mp3",
+    picture: "img/eagles.jpg",
+    singer: "EAGLES",
+  },
+  {
+    name: "Reflets D'esprit",
+    path: "music/smala.mp3",
+    picture: "img/smala.jpg",
+    singer: "LA SMALA",
+  },
+  {
     name: "Californication",
-    path: "music/californiacation.mp3",
-    picture: "img/red_hill.jpg",
+    path: "music/californication.mp3",
+    picture: "img/red2.jpg",
     singer: "RED HOT CHILI PEPPERS",
   },
   {
     name: "Pour ceux",
     path: "music/pour-ceux.mp3",
-    picture: "img/Mafia-k1-fry-logo.jpg",
+    picture: "img/mafia.jpeg",
     singer: "MAFIA K1 FRY",
   },
   {
-    name: "Je commence tout doux",
-    path: "music/je-commence.mp3",
-    picture: "img/misteryou.jpg",
-    singer: "MISTERYOU",
+    name: "Du fond du coeur",
+    path: "music/coeur.mp3",
+    picture: "img/rohff.jpg",
+    singer: "ROHFF",
   },
 ];
 
@@ -78,11 +117,13 @@ function justPlay() {
 function playSong() {
   track.play();
   playing_song = true;
+  playButton.innerHTML = '<i class="fas fa-pause"></i>';
 }
 
 function pauseSong() {
   track.pause();
   playing_song = false;
+  playButton.innerHTML = '<i class="fas fa-play"></i>';
 }
 
 function nextSong() {
@@ -109,4 +150,21 @@ function backSong() {
         player(num_index);
         playSong();
     }
+}
+function mute_sound(){
+  track.volume = 0;
+  volume.value = 0;
+  volume_show.innerHTML = 0;
+}
+function volume_change(){
+  volume_show.innerHTML = recent_volume.value;
+  track.volume = recent_volume.value / 100;
+}
+function reset_slider(){
+  slider.value = 0;
+}
+function change_duration(){
+  slider_position = track.duration * (slider.value / 100);
+  track.currentTime = slider_position;
+  //track_duration.innerHTML = slider.value;
 }
